2d9225043cd03c6584d12fc0eeee620f6a69e395
wolfdigit <wolf_digit@hotmail.com>
2019-01-19 01:23:56 +0800

    Create gst-client.bat

---
+++ client-side/gst-client.bat
@@ -0,0 +1,1 @@
+D:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe -vvv -e udpsrc port=5800 ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! avdec_h264 ! autovideosink
