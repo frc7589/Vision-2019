4dc499c1ee2328944e764ee6fa32b3b1fd3d3e11
wolfdigit <wolf_digit@hotmail.com>
2019-01-19 01:27:12 +0800

    Create gst-v4l2.sh

---
+++ Jetson-side/gst-v4l2.sh
@@ -0,0 +1,6 @@
+#gst-launch-1.0 v4l2src device=/dev/video1 ! video/x-raw,width=640,height=480 ! xvimagesink
+#gst-launch-1.0 v4l2src device=/dev/video1 ! video/x-raw,width=3840,height=2160 ! omxh264enc bitrate=300000 ! 'video/x-h264, stream-format=(string)byte-stream' ! queue max-size-buffers=0 ! h264parse ! rtph264pay ! udpsink host=172.16.21.1 port=5800
+gst-launch-1.0 v4l2src device=/dev/video1 ! video/x-raw,width=800,height=600 ! omxh264enc bitrate=300000 ! 'video/x-h264, stream-format=(string)byte-stream' ! queue max-size-buffers=0 ! h264parse ! rtph264pay ! udpsink host=172.16.21.1 port=5800
+
+
+
