80979a777e44d43155eec92f73891d164bfcfd34
someone @ Nvidia TX2 <frc@lssh.tp.edu.tw>
2019-02-03 13:19:25 +0000

    udp multicast

--- Jetson-side/gst-nvcamera.sh
+++ Jetson-side/gst-nvcamera.sh
@@ -1,2 +1,2 @@
 #gst-launch-1.0 nvcamerasrc ! nvoverlaysink -e
-gst-launch-1.0 nvcamerasrc ! omxh264enc bitrate=300000 ! 'video/x-h264, stream-format=(string)byte-stream' ! queue max-size-buffers=0 ! h264parse ! rtph264pay ! udpsink host=10.75.89.255 port=5800
+gst-launch-1.0 nvcamerasrc ! omxh264enc bitrate=7000000 ! 'video/x-h264, stream-format=(string)byte-stream' ! queue max-size-buffers=0 ! h264parse ! rtph264pay ! udpsink host=224.1.1.1 auto-multicast=true port=5800
