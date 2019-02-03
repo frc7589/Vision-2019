04ed0ec130645e677a7a415f885ec838b4042312
someone @ Nvidia TX2 <frc@lssh.tp.edu.tw>
2019-02-03 12:53:11 +0000

    braodcast ip & 2 ports

--- Jetson-side/gst-nvcamera.sh
+++ Jetson-side/gst-nvcamera.sh
@@ -1,2 +1,2 @@
 #gst-launch-1.0 nvcamerasrc ! nvoverlaysink -e
-gst-launch-1.0 nvcamerasrc ! omxh264enc bitrate=300000 ! 'video/x-h264, stream-format=(string)byte-stream' ! queue max-size-buffers=0 ! h264parse ! rtph264pay ! udpsink host=172.16.21.1 port=5800
+gst-launch-1.0 nvcamerasrc ! omxh264enc bitrate=300000 ! 'video/x-h264, stream-format=(string)byte-stream' ! queue max-size-buffers=0 ! h264parse ! rtph264pay ! udpsink host=10.75.89.255 port=5800
